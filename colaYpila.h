db984e163adb13d07b130939ead8e6f92c783519
Adrian-LP <lopezcruzjacieladrian@gmail.com>
2025-05-08 00:12:16 -0600

    Update colaYpila.h

--- colaYpila.h
+++ colaYpila.h
@@ -4,6 +4,11 @@
 #define MAX_ENCUESTA 5
 #define MAX_TEXTO 50
 
+typedef struct{
+    char estado[MAX_TEXTO];
+    char genero[MAX_TEXTO];
+}   Emp;
+
 typedef struct
 {
     char *nombre;
@@ -15,8 +20,7 @@ typedef struct
     char *telefono;
     char *departamento;
     int *respuestas;
-    char *estado;//se agrego
-    char *genero;//se agrego
+    Emp INE;//MOvimiento 1 
 } Empleados;
 
 typedef struct {
@@ -46,6 +50,7 @@ void listarArchivo(ColaCircular colaC, FILE *archivo);
 void liberarMemoriaCola(ColaCircular *colaC);
 void encuesta(ColaCircular *colaC);
 void validarEmpleados(ColaCircular *colaC, Pila *pilaAceptados, Pila *pilaRechazados,int *aceptados, int *rechazados);
+Empleados buscar(ColaCircular *colaC, int ID); // FUNCION BUSCAR 
 
 //OPERACION DEL TAD PILA
 void push(Empleados dato, Pila *pila);
@@ -59,11 +64,11 @@ void liberarMemoriaPila(Pila *pila);
 
 //ESTADISTICAS
 void mostrarPorcentajes(int aceptados, int rechazados, FILE *archivo);// se agrega la funcion para mostra la primera estadisticaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-void mostrarPromedio(ColaCircular *colaC, FILE *archivo);// se agregó la funcion para dar el promedio de las edades de los candidatos___________________________________________________
+void mostrarPromedio(ColaCircular *colaC, FILE *archivo);// se agreg� la funcion para dar el promedio de las edades de los candidatos___________________________________________________
 void aceptadosYrechazados(Pila pilaAceptados, Pila pilaRechazados, FILE *archivo);
 void promedioSalario(ColaCircular *colaC, FILE *archivo);
 void salariosExpec(ColaCircular *colaC, FILE *archivo);
-void mostrarEdadRango(ColaCircular *colaC, FILE *archivo);//Se agrego para el rango de 17 a 45 
+void mostrarEdadRango(ColaCircular *colaC, FILE *archivo);//Se agrego para el rango de 17 a 45
 void contarGeneros(ColaCircular *colaC, FILE *archivo);
 void mostrarEstados(ColaCircular *colaC, FILE *archivo);
 void carrera(ColaCircular *cola, FILE *archivo);
