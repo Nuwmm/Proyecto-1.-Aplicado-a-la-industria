7d80f02e224e348258e50314067c0c1c5f4528fa
Adrian-LP <lopezcruzjacieladrian@gmail.com>
2025-05-08 00:05:41 -0600

    main.c

--- main.c
+++ main.c
@@ -6,35 +6,35 @@
 
 int main()
 {
-    int opcion, numEmpleados, validarEncuesta=0;
+    int opcion,opcion2, numEmpleados;
     // SOLICITAR DATOS
     printf("\nEmpleados que se registraran: ");
     scanf("%d", &numEmpleados);
     ColaCircular *colaEmpleado = crearColaCircular(numEmpleados);
     Pila *pilaAceptados = crearPila(numEmpleados);
     Pila *pilaRechazados = crearPila(numEmpleados);
-    int a = 0, r = 0; // contadores para los porcentajes de aceptados y rechazados 
+    int a = 0, r = 0; // contadores para los porcentajes de aceptados y rechazados
     FILE *archivo;
-    char linea[100];
+    char linea[100]; //cantidad maxima de caracteres que peude leer la funcion fgets 
 
-    archivo = fopen("empleo.txt", "r"); //Se abrió el archivo con r para que solo sea lectura y no se modifique o se borre. 
+    archivo = fopen("empleo.txt", "r"); //Se abrio el archivo con r para que solo sea lectura y no se modifique o se borre.
 
     if (archivo == NULL) {
         printf("El archivo no existe. . .\n");
         exit (-1);
     }
-    
+
     while (fgets(linea, sizeof(linea), archivo)) {
-        printf("%s", linea); // se mandó a imprimir todo el contenido del archivo txt 
+        printf("%s", linea); // se mando a imprimir todo el contenido del archivo txt linea por linea
     }
 
-    fclose(archivo); // archivo de referencia de habilidades y vacantes 
+    fclose(archivo); // se cierra el archivo y se guarda en la corrida
 
     do
     {
         printf("\n\n====Sistema de Gestion de Solicitudes de Empleo====\n");
         opcion = desplegarMenu("\n1)Ingresar un nuevo empleado\n2)Realizar encuesta\n3)Realizar validacion\n4)Listar empleados\n5)Opciones de la Cola y Pila\n6)Mostrar las estadisticas de los empleados\n7)Salir\n\nOpcion: ", 7);
-        switch (opcion) // se le agregó al switch una opcion mas para la funcion estadistica, de momento solo va a ser una pero se le van a meter mas. _____________________________________________________________________________________________
+        switch (opcion) // se le agreg� al switch una opcion mas para la funcion estadistica, de momento solo va a ser una pero se le van a meter mas. _____________________________________________________________________________________________
         {
             case 1:
                 if(validarEspacio(*colaEmpleado))
@@ -46,27 +46,18 @@ int main()
                 listar(*colaEmpleado);
                 break;
             case 2:
-                if(!validarVacio(*colaEmpleado)){
-                    encuesta(colaEmpleado);
-                    validarEncuesta=1;
-                }
-                else
-                    printf("No hay aspirantes registrados. . .");
+                encuesta(colaEmpleado);
                 break;
             case 3:
-                if(validarEncuesta){
-                    validarEmpleados(colaEmpleado, pilaAceptados, pilaRechazados, &a, &r);// se pasa la direccion de memoria de a y r pero son obsoletas
-                    printf("\nValidacion realizada con exito. . .");
-                }
-                else
-                    printf("No se ha realizado la encuesta. . .");
+                validarEmpleados(colaEmpleado, pilaAceptados, pilaRechazados, &a, &r);// se pasa la direccion de memoria de a y r para que se guarde el contador en al variable
+                printf("\nValidacion realizada con exito. . .");
                 break;
             case 4:
                 listar(*colaEmpleado);
                 break;
             case 5:
-                opcion = desplegarMenu("\nMenu de la Cola y Pila\n\n1)Borrar elemento de la Cola\n2)Listar Cola\n3)Borrar elemento de la Pila Aceptados\n4)Borrar elementos de la Pila Rechados\n5)Listar Pila Aceptados\n6)Listar Pila Rechazados\n7)Salir\n\nOpcion: ", 7);
-                switch (opcion)
+                opcion2 = desplegarMenu("\nMenu de la Cola y Pila\n\n1)Borrar elemento de la Cola\n2)Listar Cola\n3)Borrar elemento de la Pila Aceptados\n4)Borrar elementos de la Pila Rechados\n5)Listar Pila Aceptados\n6)Listar Pila Rechazados\n7)Buscar Empleado\n\nOpcion: ", 8);
+                switch (opcion2)
                 {
                     case 1:
                         if(!validarVacio(*colaEmpleado))
@@ -97,32 +88,38 @@ int main()
                         if(!pilaVacia(*pilaRechazados))
                             listarPila(*pilaRechazados);
                         break;
-                }
-                break;
+                    case 7: 
+                        if (!validarVacio(*colaEmpleado)) {
+                            int ID;
+                            printf("Ingresa el ID de empleado:");
+                            scanf("%d",&ID);
+                            Empleados empleado=buscar(colaEmpleado,ID);
+                            listarEmpleado(empleado);
+                           
+                        }else{
+                             printf("No hay datos en la cola.\n");
+                        }
+                        break;
+                }   
             case 6:
-                if(validarEncuesta){
-                    archivo=fopen("Estadisticas.txt", "w");
-                    if(archivo!=NULL){
-                        mostrarPorcentajes(a, r, archivo); // se va a llamar la funcion para mostrar la primera estadistica #1
-                        aceptadosYrechazados(*pilaAceptados, *pilaRechazados, archivo); // #2
-                        mostrarPromedio(colaEmpleado, archivo); // #3
-                        promedioSalario(colaEmpleado, archivo);// hay qe meter estas estadisticas al archivo txt e imprimirlo #4
-                        mostrarEdadRango(colaEmpleado, archivo);//rango de edades de 17 a 45 #5
-                        mostrarEstados(colaEmpleado, archivo);//Estado de origen #6
-                        contarGeneros(colaEmpleado, archivo);//Genero de los empleados #7
-                        salariosExpec(colaEmpleado, archivo); //Porcentajes que indican cuanto porcentaje ganan de mas sobre respectivamente sobre el salario minimo #8
-                        carrera(colaEmpleado, archivo); // #9
-                        generoMayorP(colaEmpleado, archivo); // #10
-                    }
-                    else{
-                        printf("El archivo no existe. . .\n");
-                        exit(-1);
-                    }
-                    fclose(archivo);
+                archivo=fopen("Estadisticas.txt", "w");
+                if(archivo!=NULL){
+                    mostrarPorcentajes(a, r, archivo); // se va a llamar la funcion para mostrar la primera estadistica #1
+                    aceptadosYrechazados(*pilaAceptados, *pilaRechazados, archivo); // #2
+                    mostrarPromedio(colaEmpleado, archivo); // #3
+                    promedioSalario(colaEmpleado, archivo);// hay qe meter estas estadisticas al archivo txt e imprimirlo #4
+                    mostrarEdadRango(colaEmpleado, archivo);//rango de edades de 17 a 45 #5
+                    mostrarEstados(colaEmpleado, archivo);//Estado de origen #6
+                    contarGeneros(colaEmpleado, archivo);//Genero de los empleados #7
+                    salariosExpec(colaEmpleado, archivo); //Porcentajes que indican cuanto porcentaje ganan de mas sobre respectivamente sobre el salario minimo #8
+                    carrera(colaEmpleado, archivo); // #9
+                    generoMayorP(colaEmpleado, archivo); // #10
                 }
-                else
-                    printf("No se ha realizado la encuesta. . .");
-                break;
+                else{
+                    printf("El archivo no existe. . .\n");
+                    exit(-1);
+                }
+                fclose(archivo);
         }
     }while(opcion!=7); //Se va a ir modificando
     if(colaEmpleado->h<=colaEmpleado->t)
